35781460687b32acf4126856ac02404a6c5551d0
misschip <misschip@naver.com>
2020-08-18 18:20:21 +0900

    LiveRe Social Reply System

---
+++ src/test/java/com/cos/livere/LivereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cos.livere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
